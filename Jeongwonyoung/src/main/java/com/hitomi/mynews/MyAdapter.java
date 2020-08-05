eecfd48a10f541dd92bb01e44bfbb6776c0b6159
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2020-08-05 22:57:46 +0900

    Update MyAdapter.java

--- Jeongwonyoung/src/main/java/com/hitomi/mynews/MyAdapter.java
+++ Jeongwonyoung/src/main/java/com/hitomi/mynews/MyAdapter.java
@@ -5,7 +5,7 @@ import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.widget.ImageView; 
 
 import android.widget.TextView;
 
