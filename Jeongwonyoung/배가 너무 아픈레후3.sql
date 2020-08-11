5f40abc8ccbbd3c7ae73fbdf0946fe7d3f467418
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2020-08-11 14:31:23 +0900

    Update 배가 너무 아픈레후3.sql

--- Jeongwonyoung/배가 너무 아픈레후3.sql
+++ Jeongwonyoung/배가 너무 아픈레후3.sql
@@ -13,3 +13,9 @@ grant select on 학사DB.instructor TO 'student'@'%';
 show grants for student;
 
 create user 'professor'@'%' identified by 'professor';
+grant SELECT on 학사DB.* to 'professor'@'%';
+grant INSERT, UPDATE on 학사DB.course_taken to 'professor'@'%';
+
+show grants for professor;
+
+drop user academicManager@localhost;
