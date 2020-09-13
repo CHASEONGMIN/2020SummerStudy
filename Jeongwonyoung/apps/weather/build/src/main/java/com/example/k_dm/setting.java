8eb0108aec2faf8e0b3da17e9b9f2eacb779edc0
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2020-09-13 11:50:59 +0900

    Update setting.java

--- Jeongwonyoung/apps/weather/build/src/main/java/com/example/k_dm/setting.java
+++ Jeongwonyoung/apps/weather/build/src/main/java/com/example/k_dm/setting.java
@@ -115,30 +115,31 @@ public class setting extends Activity {
                 ((MainActivity) MainActivity.Context).homesi = choice_do;
                 shared.setString(MainActivity.Context, "homesi", choice_do);
                 ((MainActivity) MainActivity.Context).homegun = choice_se;
+                
                 shared.setString(MainActivity.Context, "homegun", choice_se);
             }
         });
 
 
-        spin3.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {//첫번째 spinner 클릭시 이벤트 발생. setO 정도까지 치시면 자동완성
+        spin3.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {//첫번째 spinner 클릭시 이벤트 발생.자동완성
             @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {//int i는 포지션이라 하여 제가 spinner에 몇번째걸 선택했는지 값이 들어감
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {//int i는 포지션(spinner에 몇번째걸 선택했는지 값이 들어감)
                 if (adspin3.getItem(i).equals("서울특별시")) {//spinner에 값을 가져와서 i 클릭 한것이 서울인지 확인
-                    choice_do = "서울특별시";//버튼 클릭시 출력을 위해 값을 넣었습니다.
-                    adspin4 = ArrayAdapter.createFromResource(setting.this, R.array.spinner_do_seoul, android.R.layout.simple_spinner_dropdown_item);//서울일 경우에 두번째 spinner에 값 삽입 //그냥 this가 아닌 Main~~~인 이유는 그냥 this는 메인엑티비티인 경우만 가능합니다. //지금과 같이 다른 함수안이나 다른 클래스에서는 꼭 자신의 것을 넣어주어야 합니다.//혹시나 다른 class -> Public View밑에서 작업하시는 분은 view명.getContext()로 해주셔야 합니다.//예로 View rootView =~~ 선언하신 경우에는 rootView.getContext()써주셔야합니다. this가 아니라요.
+                    choice_do = "서울특별시";//버튼 클릭시 출력 
+                    adspin4 = ArrayAdapter.createFromResource(setting.this, R.array.spinner_do_seoul, android.R.layout.simple_spinner_dropdown_item);//서울일 경우에 두번째 spinner에 값 삽입 //그냥 this가 아닌 Main~~~인 이유는 그냥 this는 메인엑티비티인 경우만 가능. //지금과 같이 다른 함수안이나 다른 클래스에서는 꼭 자신의 것을 넣어주어야 합니다.//혹시나 다른 class -> Public View밑에서 작업하시는 분은 view명.getContext()로 해주셔야 합니다.//예로 View rootView =~~ 선언하신 경우에는 rootView.getContext()써주셔야합니다. this가 아니라요.
                     adspin4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                    spin4.setAdapter(adspin4);//두번째 어댑터값을 두번째 spinner에 넣었습니다.
+                    spin4.setAdapter(adspin4);//두번째 어댑터값을 두번째 spinner에
                     spin4.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {//두번째 선택된 값도 필요하니 이안에 두번째 spinner 선택 이벤트를 정의
                         @Override
                         public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                            choice_se = adspin4.getItem(i).toString();//두번째 선택된 값을 choice_se에 넣습니다.
+                            choice_se = adspin4.getItem(i).toString();//두번째 선택된 값을 choice_se 삽입
                         }
 
                         @Override
-                        public void onNothingSelected(AdapterView<?> adapterView) {//아무것도 선택안될시 부분입니다. 자동완성됩니다.
+                        public void onNothingSelected(AdapterView<?> adapterView) {//아무것도 선택안될시 부분
                         }
                     });
-                } else if (adspin3.getItem(i).equals("인천광역시")) {//똑같은 소스에 반복입니다. 인천부분입니다.
+                } else if (adspin3.getItem(i).equals("인천광역시")) {//똑같은 소스에 반복 (인천부분)
                     choice_do = "인천광역시";
                     adspin4 = ArrayAdapter.createFromResource(setting.this, R.array.spinner_do_incheon, android.R.layout.simple_spinner_dropdown_item);
                     adspin4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -153,7 +154,7 @@ public class setting extends Activity {
                         public void onNothingSelected(AdapterView<?> adapterView) {
                         }
                     });
-                } else if (adspin3.getItem(i).equals("경기도")) {//똑같은 소스에 반복입니다. 인천부분입니다.
+                } else if (adspin3.getItem(i).equals("경기도")) {//똑같은 소스에 반복 (인천)
                     choice_do = "경기도";
                     adspin4 = ArrayAdapter.createFromResource(setting.this, R.array.spinner_do_gg, android.R.layout.simple_spinner_dropdown_item);
                     adspin4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -178,7 +179,7 @@ public class setting extends Activity {
 
             }
         });
-        btn_refresh2.setOnClickListener(new View.OnClickListener() {//버튼 클릭시 이벤트입니다.
+        btn_refresh2.setOnClickListener(new View.OnClickListener() {//버튼 클릭시 이벤트
             @Override
             public void onClick(View view) {
 
